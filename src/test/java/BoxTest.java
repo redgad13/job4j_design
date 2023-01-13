fa040e081ec0c321f49c754f8378577c476a6035
redgad13 <vadim_kuzin@list.ru>
2023-01-13 13:24:47 +0300

    Утверждения с примитивными типами [#504883]

---
+++ src/test/java/BoxTest.java
@@ -0,0 +1,61 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class BoxTest {
+    @Test
+    void isThisSphere() {
+        Box box = new Box(0, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Sphere");
+    }
+
+    @Test
+    void isThisTetrahedron() {
+        Box box = new Box(4, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Tetrahedron");
+    }
+
+    @Test
+    void whenSphereThen0Verticies() {
+        Box box = new Box(0, 4);
+        int result = 0;
+        assertThat(result).isEqualTo(box.getNumberOfVertices());
+    }
+
+    @Test
+    void whenCubeThen8Verticies() {
+        Box box = new Box(8, 4);
+        int result = 8;
+        assertThat(result).isEqualTo(box.getNumberOfVertices());
+    }
+
+    @Test
+    void whenExists() {
+        Box box = new Box(4, 12);
+        boolean rsl = box.isExist();
+        assertThat(rsl).isTrue();
+    }
+
+    @Test
+    void whenDoesntExist() {
+        Box box = new Box(1, 12);
+        boolean rsl = box.isExist();
+        assertThat(rsl).isFalse();
+    }
+
+    @Test
+    void whenVertex0Edge3ThenAreaEquals113() {
+        Box box = new Box(0, 3);
+        double rsl = 4 * Math.PI * (3 * 3);
+        assertThat(rsl).isEqualTo(box.getArea());
+    }
+
+    @Test
+    void whenVertex4Edge5ThenAreaEquals51() {
+        Box box = new Box(4, 5);
+        double rsl = Math.sqrt(3) * (5 * 5);
+        assertThat(rsl).isEqualTo(box.getArea());
+    }
+}
