0f24973b3ad448d15fb3f035dc636805e40826df
redgad88 <vadim_kuzin@list.ru>
2023-01-25 17:39:40 +0300

    3. Удалить head в односвязном списке. [#51424]

---
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -0,0 +1,87 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class ForwardLinked<T> implements Iterable<T> {
+    private int size = 0;
+    private int modCount = 0;
+    private Node<T> head;
+
+    public void add(T value) {
+        Node<T> newNode = new Node<>(value, null);
+        Node<T> current = head;
+        if (head == null) {
+            head = newNode;
+        } else {
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        modCount++;
+        size++;
+    }
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        Node<T> current = head;
+        T value = null;
+        for (int i = 0; i <= index; i++) {
+            value = current.item;
+            current = current.next;
+        }
+        return value;
+    }
+
+    public T deleteFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        T rsl = head.item;
+        Node<T> newHead = head.next;
+        head.next = null;
+        head = newHead;
+        size --;
+        modCount++;
+        return rsl;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            Node<T> current = head;
+            final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return current != null;
+            }
+
+            @Override
+            public T next() {
+                Node<T> rsl = current;
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                current = current.next;
+                return rsl.item;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        private final T item;
+        private Node<T> next;
+
+        public Node(T item, Node<T> next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+}
