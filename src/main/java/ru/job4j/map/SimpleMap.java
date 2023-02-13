921dc80364863961324bfe9e2462dda7d7620417
redgad88 <vadim_kuzin@list.ru>
2023-02-13 15:24:15 +0300

    10. Реализовать собственную структуру данных - HashMap [#1008]

---
+++ src/main/java/ru/job4j/map/SimpleMap.java
@@ -0,0 +1,99 @@
+package ru.job4j.map;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+public class SimpleMap<K, V> implements Map<K, V> {
+
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private int capacity = 8;
+
+    private int count = 0;
+
+    private int modCount = 0;
+
+    private MapEntry<K, V>[] table = new MapEntry[capacity];
+
+    @Override
+    public boolean put(K key, V value) {
+        boolean rsl = false;
+        if (count >= capacity * LOAD_FACTOR) {
+            expand();
+        }
+        int hashCode = key.hashCode();
+        int hash = hash(hashCode);
+        int index = indexFor(hash);
+        if (table[index] == null) {
+            table[index] = new MapEntry<>(key, value);
+            rsl = true;
+        }
+        count++;
+        modCount++;
+        return rsl;
+    }
+
+    private int hash(int hashCode) {
+        return hashCode ^ (hashCode >>> 16);
+    }
+
+    private int indexFor(int hash) {
+        return hash & (capacity - 1);
+    }
+
+    private void expand() {
+        capacity = capacity * 2;
+    }
+
+    @Override
+    public V get(K key) {
+        for (MapEntry<K, V> kvMapEntry : table) {
+            if (kvMapEntry.key.equals(key)){
+                return kvMapEntry.value;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean remove(K key) {
+        for (MapEntry<K, V> kvMapEntry : table) {
+            if (kvMapEntry.key.equals(key)){
+                kvMapEntry = null;
+                break;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return new Iterator<>() {
+            int point;
+            final int expectedModCount = modCount;
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return point < count;
+            }
+
+            @Override
+            public K next() {
+                return table[point++].key;
+            }
+        };
+    }
+
+    private static class MapEntry<K, V> {
+
+        K key;
+        V value;
+
+        public MapEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
