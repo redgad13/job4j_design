da3d34cdfa61960c25cd73142bdeca184d5a7c74
redgad13 <vadim_kuzin@list.ru>
2023-03-09 20:24:33 +0300

    6. Читаем файл конфигурации [#858 #398520]

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -25,18 +25,14 @@ public class Config {
                     continue;
                 }
                 lines = line.split("=", 2);
-                if (lines.length > 1) {
+                if (lines.length == 2 && (!lines[0].isBlank() && !lines[1].isBlank())) {
                     String key = lines[0];
                     String value = lines[1];
                     values.put(key, value);
-                }
-            }
-            for (String s : lines) {
-                if (s.equals("") || lines.length < 2) {
+                } else {
                     throw new IllegalArgumentException();
                 }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
