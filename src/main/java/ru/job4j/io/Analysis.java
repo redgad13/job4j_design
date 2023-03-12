2db406981244475242ac218f176844b9f786d8b2
redgad13 <vadim_kuzin@list.ru>
2023-03-12 13:43:49 +0300

    7. Анализ доступности сервера [#859 #399663]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,37 +1,26 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Analysis {
     public void unavailable(String source, String target) {
-        boolean offline;
-        boolean online;
         try (BufferedReader br = new BufferedReader(new FileReader(source));
              PrintWriter pr = new PrintWriter(new FileOutputStream(target))
         ) {
-            List<String> log = new ArrayList<>();
+            boolean isOn = true;
             while (br.ready()) {
-                String line = br.readLine();
-                log.add(line);
+                String currLine = br.readLine();
+                boolean currLineIsOn = currLine.startsWith("200") || currLine.startsWith("300");
+                if (isOn != currLineIsOn) {
+                    String[] stats = currLine.split(" ");
+                    pr.write(stats[1] + ";");
+                    if (!isOn) {
+                        pr.write(System.lineSeparator());
+                    }
+                    isOn = !isOn;
+                }
             }
-            for (int i = 1; i < log.size() - 1; i++) {
-                String[] currStats = log.get(i).split(" ");
-                String[] nextStats = log.get(i + 1).split(" ");
 
-                offline = (log.get(i - 1).startsWith("200") || log.get(i - 1).startsWith("300"))
-                        && (log.get(i).startsWith("400") || log.get(i).startsWith("500"));
-                online = (log.get(i).startsWith("400") || log.get(i).startsWith("500"))
-                        && (log.get(i + 1).startsWith("200") || log.get(i + 1).startsWith("300"));
-                if (offline) {
-                    pr.write((currStats[1] + ";"));
-                }
-                if (online) {
-                    pr.write(nextStats[1]);
-                    pr.write(System.lineSeparator());
-                }
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
