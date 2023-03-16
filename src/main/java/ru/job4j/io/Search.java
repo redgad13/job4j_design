48ec0d96a6af2869c9d6d697a53e2a77043bf820
redgad13 <vadim_kuzin@list.ru>
2023-03-16 14:43:34 +0300

    10. Сканирование файловой системы [#106929 #401408]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -4,19 +4,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
         Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        search(start, p -> p.toFile().getName().endsWith(".sample")).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
-        Path rsl = Files.walkFileTree(root, searcher);
-        return searcher.getPaths(rsl);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
     }
 }
--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -1,10 +1,7 @@
 package ru.job4j.io;
 
-import jdk.dynalink.linker.LinkerServices;
-
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -14,25 +11,22 @@ import java.util.function.Predicate;
 
 public class SearchFiles extends SimpleFileVisitor<Path> {
     private Predicate<Path> predicate;
+    private List<Path> lines;
 
     public SearchFiles(Predicate<Path> predicate) {
         this.predicate = predicate;
+        lines = new ArrayList<>();
     }
 
     @Override
-    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        List<String> lines = Files.readAllLines(file);
-        for (String line : lines) {
-            if (predicate.test(file)) {
-                getPaths(Path.of(line));
-            }
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        if (predicate.test(file)) {
+            lines.add(file);
         }
         return FileVisitResult.CONTINUE;
     }
 
-    public List<Path> getPaths(Path s) {
-        List<Path> rsl = new ArrayList<>();
-        rsl.add(s);
-        return rsl;
+    public List<Path> getPaths() {
+        return lines;
     }
 }
