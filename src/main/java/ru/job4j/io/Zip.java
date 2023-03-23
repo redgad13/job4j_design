b76b51b0e90dbc36013e139f298229143d5769ed
redgad13 <vadim_kuzin@list.ru>
2023-03-23 15:48:54 +0300

    14. Архивировать проект [#861 #403527]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -3,17 +3,16 @@ package ru.job4j.io;
 import java.io.*;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public void packFiles(List<File> sources, File target) {
+    public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            for (File source : sources) {
-                zip.putNextEntry(new ZipEntry(source.getPath()));
-                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+            for (Path source : sources) {
+                zip.putNextEntry(new ZipEntry(source.toString()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source.toString()))) {
                     zip.write(out.readAllBytes());
                 }
             }
@@ -35,11 +34,8 @@ public class Zip {
 
     public static void main(String[] args) throws IOException {
         ArgsName argsName = ArgsName.of(args);
-        List<Path> paths = Search.search(Path.of(argsName.get("d")),
+        List<Path> files = Search.search(Path.of(argsName.get("d")),
                 p -> !p.toFile().getName().endsWith(argsName.get("e")));
-        List<File> files = paths.stream()
-                .map(Path::toFile)
-                .collect(Collectors.toList());
         Zip zip = new Zip();
         zip.packFiles(files, new File(argsName.get("o")));
     }
