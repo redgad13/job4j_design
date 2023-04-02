043f0cc5b48c80b4f4e99f0decc924f217336a7b
redgad13 <vadim_kuzin@list.ru>
2023-04-02 21:40:05 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -4,25 +4,28 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/*
+http://localhost:9000/?msg=Hello
+ */
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
-                     BufferedReader br = new BufferedReader(
+                     BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String s = br.readLine(); s != null && !s.isEmpty(); s = br.readLine()) {
-                        if ("Hello".contains(s)) {
-                            out.write("Hello".getBytes());
-                        }
-                        if ("Exit".contains(s)) {
-                            server.close();
-                        } else {
-                            out.write(s.substring(s.indexOf('=')).getBytes());
-                        }
-                        out.flush();
+                    out.flush();
+                    server.close();
+                    String request = in.readLine();
+                    request = request.substring((request.indexOf('=') + 1), request.indexOf("HTTP") - 1);
+                    if ("Exit".equalsIgnoreCase(request)) {
+                        out.write("".getBytes());
+                    } else if ("Hello".equalsIgnoreCase(request)) {
+                        out.write("HI HELLO".getBytes());
+                    } else {
+                        out.write(request.getBytes());
                     }
                 }
             }
