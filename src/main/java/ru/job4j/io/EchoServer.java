66859642c93bc5c8b518d9c983ba9c355a8d83cb
redgad13 <vadim_kuzin@list.ru>
2023-04-04 13:50:59 +0300

    3. Бот [#7921 #406390]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -4,12 +4,9 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-/*
-http://localhost:9000/?msg=Hello
- */
 public class EchoServer {
     public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(9000)) {
+        try (ServerSocket server = new ServerSocket(8080)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
