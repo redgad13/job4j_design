85ae08e5667c427947f7610bd049033371567ee2
redgad13 <vadim_kuzin@list.ru>
2023-04-14 11:27:10 +0300

    3. Поиск файлов по критерию [#783 #408765]

---
+++ src/main/java/ru/job4j/ioexam/FindFiles.java
@@ -0,0 +1,54 @@
+package ru.job4j.ioexam;
+
+import ru.job4j.io.ArgsName;
+import ru.job4j.io.SearchFiles;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FindFiles {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 4) {
+            throw new IllegalArgumentException("Check the quantity of parameters");
+        }
+        ArgsName argsName = ArgsName.of(args);
+        checkParams(argsName);
+        Path directory = Path.of(argsName.get("d"));
+        String outputFile = argsName.get("o");
+        List<Path> lines = findFiles(directory, p -> p.getFileName().toString().contains(argsName.get("n")));
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile))) {
+            for (Path line : lines) {
+                bufferedWriter.write(line.toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void checkParams(ArgsName argsName) {
+        File dir = new File(argsName.get("d"));
+        if (!dir.isDirectory()) {
+            throw new IllegalArgumentException(String.format("%s is not a directory", argsName.get("d")));
+        }
+        Pattern p = Pattern.compile(".*\\..*");
+        Matcher m = p.matcher(argsName.get("n"));
+        if (!m.find()) {
+            throw new IllegalArgumentException(String.format("%s is not a file", argsName.get("n")));
+        }
+        File outputFile = new File(argsName.get("o"));
+        if (!outputFile.exists()) {
+            throw new IllegalArgumentException(String.format("%s doesn't exist", argsName.get("o")));
+        }
+    }
+
+    public static List<Path> findFiles(Path start, Predicate<Path> predicate) throws IOException {
+        SearchFiles searchFiles = new SearchFiles(predicate);
+        Files.walkFileTree(start, searchFiles);
+        return searchFiles.getPaths();
+    }
+}
