25a61f5dc5857e524fd68d6711022291893036e4
redgad13 <vadim_kuzin@list.ru>
2023-04-17 18:09:51 +0300

    0. Что такое база данных. [#6860 #410290]

---
+++ db/hotel_staff.sql
@@ -0,0 +1,4 @@
+insert into staff(name, title, salary) values('Vadim', 'manager', 99999);
+insert into staff(name, title, salary) values('Andrei', 'accountant', 7777);
+insert into staff(name, title, salary) values('Anna', 'maid', 11);
+select * from staff;
