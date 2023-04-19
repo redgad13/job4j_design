49710cbd7697d353d1922829e3e71b15c874fc45
redgad13 <vadim_kuzin@list.ru>
2023-04-19 15:05:43 +0300

    0.1. Связи между таблицами [#373615]

---
+++ src/main/java/sql/create.sql
@@ -0,0 +1,54 @@
+create table master(
+id serial primary key,
+master_name varchar(15),
+pets_quantity int
+);
+
+create table pet(
+id serial primary key,
+nickname varchar(10)
+);
+
+create table pet_owner(
+id serial primary key,
+master_id int references master(id),
+pet_id int references pet(id)
+);
+
+insert into master(master_name) values ('John');
+insert into master(master_name) values ('Bill');
+insert into master(master_name) values ('Joe');
+insert into pet(nickname) values ('Zhuchka');
+insert into pet(nickname) values ('Belka');
+insert into pet(nickname) values ('Strelka');
+insert into pet(nickname) values ('Malish');
+insert into pet_owner(master_id, pet_id) values (1, 1);
+insert into pet_owner(master_id, pet_id) values (2, 2);
+insert into pet_owner(master_id, pet_id) values (3, 3);
+insert into pet_owner(master_id, pet_id) values (3, 4);
+
+select * from pet_owner;
+
+create table dog_master(
+id serial primary key,
+dm_name varchar(15),
+dogs_qnty int
+);
+
+create table dogs(
+id serial primary key,
+nickname varchar(12),
+master_id int references dog_master(id)
+);
+
+alter table dogs rename column dog_id to master_id;
+
+insert into dog_master(dm_name) values ('First master');
+insert into dog_master(dm_name) values ('Second master');
+insert into dogs (nickname, master_id) values ('First dog', 1);
+insert into dogs (nickname, master_id) values ('Second dog', 1);
+insert into dogs (nickname, master_id) values ('Third dog', 2);
+insert into dogs (nickname, master_id) values ('Fourth dog', 2);
+insert into dogs (nickname, master_id) values ('Fifth dog', 2);
+select * from dogs;
+
