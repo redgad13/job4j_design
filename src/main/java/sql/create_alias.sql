236384ff27273519516787fdfdfe453edc67a042
redgad13 <vadim_kuzin@list.ru>
2023-04-23 12:15:43 +0300

    1. Inner join [#373617]

---
+++ src/main/java/sql/create_alias.sql
@@ -0,0 +1,1 @@
+create database alias_db;
---
+++ src/main/java/sql/insert_alias.sql
@@ -0,0 +1,25 @@
+create table producer(
+id serial primary key,
+title text
+);
+
+create table computer(
+id serial primary key,
+model text,
+producer_id int references producer(id)
+);
+
+insert into producer(title) values ('apple');
+insert into producer(title) values ('intel');
+
+insert into computer(model, producer_id) values ('macintosh', 1);
+insert into computer(model, producer_id) values ('pentium-celeron', 2);
+insert into computer(model, producer_id) values ('super macintosh', 1);
+insert into computer(model, producer_id) values ('pentium', 2);
+insert into computer(model, producer_id) values ('celeron', 2);
+
+select p.title, c.model, c.producer_id
+from computer c join producer p on c.producer_id = p.id;
+
+select p.title, c.model
+from computer c join producer p on c.producer_id = p.id;
