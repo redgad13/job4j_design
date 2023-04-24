aa67f4dd9ce93c43a050c94bc5e14db154de122a
redgad13 <vadim_kuzin@list.ru>
2023-04-24 16:36:39 +0300

    3. Фильтры [#1732]

--- src/main/java/sql/insert_product.sql
+++ src/main/java/sql/insert_product.sql
@@ -11,12 +11,6 @@ expired_date date,
 price decimal(10, 2)
 );
 
-create table product_with_types(
-id serial primary key,
-type_id int references type(id),
-product_id int references product(id)
-);
-
 insert into type (type_name) values ('СЫР'), ('МОЛОКО');
 
 insert into product(product_name, type_id, expired_date, price) values
@@ -25,14 +19,27 @@ insert into product(product_name, type_id, expired_date, price) values
 ('мороженое', 2, '21/04/2023', 120), ('сметана', 2, '21/05/2023', 60),
 ('творог', 2, '21/06/2023', 160), ('кефир', 2, '21/05/2022', 100);
 
-select p.product_name as Продукт
-from product_with_types as pwt
-join product as p on p.id = pwt.product_id
-join type as t on t.id = pwt.type_id
-where t.type_name like 'СЫР';
-
-select product_name from product where type_id = 1;
-
+select product_name as Продукт from product where type_id = 1;
 select product_name as Продукт from product where product_name like '%мороженое%';
 select product_name as Продукт, expired_date as Просрочка from product where expired_date < CURRENT_DATE;
+select product_name as Продукт from product where price = (select max(price) from product);
+
+select type_name, count(product_name)
+from type
+join product on type.id = product.type_id
+group by type_name;
+
+select product_name
+from product
+where type_id in (select id from type where type_name in ('СЫР', 'МОЛОКО'));
+
+select type_name, count(product_name)
+from type
+join product on product.type_id = type.id
+group by type_name
+having count(product_name) < 10;
+
+select product_name, type.type_name
+from product
+join type on product.type_id = type.id;
 
