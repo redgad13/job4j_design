bec59faca46ddac4cbdb9fc87a0bf6b9a83a184e
redgad13 <vadim_kuzin@list.ru>
2023-04-28 18:21:07 +0300

    Хранилище машин [#1733]

--- src/main/java/sql/insert_cars.sql
+++ src/main/java/sql/insert_cars.sql
@@ -31,13 +31,32 @@ insert into car_bodies(body_name) values ('that very body');
 insert into cars(car_name, body_id, engine_id, transmission_id) values
 ('fifth car', 5, 1, 1);
 
+insert into cars(car_name, body_id, engine_id, transmission_id) values
+('last car', 1 , null , null);
+insert into car_bodies(body_name) values ('not used body');
+insert into car_engines(engine_name) values ('not used engine');
+insert into car_transmissions(transmission_name) values ('not used transmission');
+
 select car_name as Машина, engine_name as Двигатель, body_name as Кузов, transmission_name as Коробка
 from cars c
 left join car_engines ce on ce.id = c.engine_id
 left join car_bodies cb on cb.id = c.body_id
 left join car_transmissions ct on ct.id = c.transmission_id;
 
-select body_name
-from cars c
-left join car_bodies cb on cb.id = c.body_id
-where body_name LIKE 'that very body';
+select cb.body_name, c.car_name
+from car_bodies as cb
+left join cars as c
+on cb.id = c.body_id
+where c.car_name is null;
+
+select ct.transmission_name, c.car_name
+from car_transmissions as ct
+left join cars as c
+on ct.id = c.transmission_id
+where c.car_name is null;
+
+select ce.engine_name, c.car_name
+from car_engines as ce
+left join cars as c
+on ce.id = c.engine_id
+where c.car_name is null;
