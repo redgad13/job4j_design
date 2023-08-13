df558b15cd392406b17e964905425586ea6d28cd
redgad88 <vadim_kuzin@list.ru>
2023-08-13 08:19:10 +0300

    4. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.template;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+class GeneratorTest {
+    @Test
+    public void whenProduceIsOk() {
+    Generator generator = new GeneratorForName();
+    Map<String, String> map = new HashMap<>();
+    map.put("Petr Arsentev", "you");
+    String name = generator.produce("I am a ${name}, Who are ${subject}? ", map);
+    assertThat(generator.produce(name, map)).isEqualTo("I am a Petr Arsentev, Who are you? ");
+    }
+
+}
