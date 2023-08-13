1fc0009e70511834de167980fcee72c81bdc6cea
redgad88 <vadim_kuzin@list.ru>
2023-08-13 08:09:16 +0300

    3. Что такое TDD? [#4918 #436456]

---
+++ src/main/java/ru/job4j/ood/tdd/Cinema3D.java
@@ -0,0 +1,24 @@
+package ru.job4j.ood.tdd;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Cinema3D implements Cinema {
+
+    @Override
+    public List<Session> find(Predicate<Session> filter) {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public Ticket buy(Account account, int row, int column, Calendar date) {
+        return new Ticket3D();
+    }
+
+    @Override
+    public void add(Session session) {
+
+    }
+}
