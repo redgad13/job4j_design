caa069082a71d980aa2e01e4f433ce03d6cb9eec
redgad88 <vadim_kuzin@list.ru>
2023-08-21 21:18:40 +0300

    1. Поддержка форматов XML, JSON в генераторе отчетов. [#851]

--- src/test/java/ru/job4j/ood/srp/report/JSONReportTest.java
+++ src/test/java/ru/job4j/ood/srp/report/JSONReportTest.java
@@ -9,6 +9,7 @@ import ru.job4j.ood.srp.model.Employee;
 import ru.job4j.ood.srp.store.MemStore;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -17,12 +18,19 @@ class JSONReportTest {
     public void whenOldGenerated() {
         MemStore store = new MemStore();
         Calendar now = Calendar.getInstance();
-        Employee worker = new Employee("Ivan", now, now, 100);
+        Employee worker = new Employee("Ivan", new GregorianCalendar(2020, 0, 15, 1, 11),
+                new GregorianCalendar(2021, 0, 15, 1, 11), 100);
+        Employee worker1 = new Employee("Petr", new GregorianCalendar(2021, 1, 16, 2, 22),
+                new GregorianCalendar(2022, 0, 15, 1, 11), 200);
+        Employee worker2 = new Employee("Sergei", new GregorianCalendar(2022, 2, 17, 3, 3),
+                new GregorianCalendar(2023, 0, 15, 1, 11), 300);
         DateTimeParser<Calendar> parser = new ReportDateTimeParser();
         Gson gson = new GsonBuilder().create();
         store.add(worker);
         Report engine = new JSONReport(store, parser, gson);
-        StringBuilder expect = new StringBuilder();
+        String expect = "Name; Hired; Fired; Salary;"
+                + System.lineSeparator()
+                + "[{\"name\":\"Ivan\",\"hired\":{\"year\":2020,\"month\":0,\"dayOfMonth\":15,\"hourOfDay\":1,\"minute\":11,\"second\":0},\"fired\":{\"year\":2021,\"month\":0,\"dayOfMonth\":15,\"hourOfDay\":1,\"minute\":11,\"second\":0},\"salary\":100.0}]";
         assertThat(engine.generate(em -> true)).isEqualTo(expect);
     }
 
