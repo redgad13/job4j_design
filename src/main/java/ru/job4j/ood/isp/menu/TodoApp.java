b36931cbe2d4d2cdb5f3e8db0c1828731c26886b
redgad88 <vadim_kuzin@list.ru>
2023-10-18 17:29:53 +0300

    1. Создать меню. [#4748]

--- src/main/java/ru/job4j/ood/isp/menu/TodoApp.java
+++ src/main/java/ru/job4j/ood/isp/menu/TodoApp.java
@@ -15,7 +15,7 @@ public class TodoApp {
         TodoApp todoApp = new TodoApp();
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             line = br.readLine();
-            while (!line.equals("exit")) {
+            while (!line.equals("выход")) {
                 todoApp.addParent(line);
             }
         } catch (IOException e) {
@@ -29,12 +29,12 @@ public class TodoApp {
 
     private void addParent(String child) {
         menu.add(Menu.ROOT, child, STUB_ACTION);
-        System.out.println("would you like to add child item? yes/no");
+        System.out.println("добавить подпункт меню? да/нет");
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             String answer = br.readLine();
             while ("yes".equals(answer)) {
                 addChild(child);
-                System.out.println("would you like to add more child item? yes/no");
+                System.out.println("добавить еще подпункт да/нет");
                 answer = br.readLine();
             }
         } catch (IOException e) {
@@ -44,7 +44,7 @@ public class TodoApp {
 
     private void addChild(String parent) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            System.out.println("enter child name");
+            System.out.println("введите название подпункта");
             String child = br.readLine();
             menu.add(parent, child, STUB_ACTION);
         } catch (IOException e) {
@@ -54,8 +54,8 @@ public class TodoApp {
 
     private static void message() {
         System.out.println("""
-                enter menu item:
-                to exit enter "exit"
+                Введите элемент меню:
+                Для выхода введите "выход"
                 """);
     }
 
